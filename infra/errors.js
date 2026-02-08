541029cef329f116c0ced5d534af59e39688b972
Yale Henrique Araújo dos Santos <yale850@gmail.com>
2026-02-08 12:33:01 -0300

    feat: create and use `NotFoundError` in `controller.js`

--- infra/errors.js
+++ infra/errors.js
@@ -75,3 +75,23 @@ export class ValidationError extends Error {
     };
   }
 }
+
+export class NotFoundError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "O recurso solicitado não foi encontrado.", { cause });
+
+    this.name = "NotFoundError";
+    this.action = action || "Verifique se o recurso existe e tente novamente.";
+
+    this.statusCode = 404;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
