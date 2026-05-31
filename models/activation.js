5c93d66265e5156b2cf91d72114d3119e34376b5
Yale Araújo <yale850@gmail.com>
2026-05-31 15:31:41 -0300

    feat: require `read:activation_token` to access `/api/v1/activations/[token_id]`

--- models/activation.js
+++ models/activation.js
@@ -1,12 +1,14 @@
 import database from "infra/database";
 import email from "infra/email";
+import { ForbiddenError, NotFoundError } from "infra/errors";
 import webserber from "infra/webserver";
+import authorization from "models/authorization";
 import user from "models/user";
 
-const EXPIRATION_IN_MILLISECONDS = 1000 * 60 * 15; // 15 minutes
+const EXPIRATION_IN_MILISECONDS = 1000 * 60 * 15; // 15 minutes
 
 async function create(userId) {
-  const expiresAt = new Date(Date.now() + EXPIRATION_IN_MILLISECONDS);
+  const expiresAt = new Date(Date.now() + EXPIRATION_IN_MILISECONDS);
 
   const newToken = await runInsertQuery(userId, expiresAt);
   return newToken;
@@ -48,19 +50,27 @@ async function findOneValidById(tokenId) {
   async function runSelectQuery(tokenId) {
     const result = await database.query({
       text: `
-      SELECT 
+      SELECT
         *
-      FROM 
+      FROM
         user_activation_tokens
-      WHERE 
+      WHERE
         id = $1
         AND expires_at > NOW()
         AND used_at IS NULL
-      ORDER BY 
+      ORDER BY
         created_at DESC
       LIMIT 1`,
       values: [tokenId],
     });
+
+    if (result.rowCount === 0) {
+      throw new NotFoundError({
+        message:
+          "O token de ativação utilizado não foi encontrado no sistema ou expirou.",
+        action: "Faça um novo cadastro.",
+      });
+    }
 
     return result.rows[0];
   }
@@ -90,6 +100,15 @@ async function markTokenAsUsed(activationTokenId) {
 }
 
 async function activateUserByUserId(userId) {
+  const userToActivate = await user.findOneById(userId);
+
+  if (!authorization.can(userToActivate, "read:activation_token")) {
+    throw new ForbiddenError({
+      message: "Você não pode mais utilizar tokens de ativação.",
+      action: "Entre em contato com o suporte.",
+    });
+  }
+
   const activatedUser = await user.setFeatures(userId, [
     "create:session",
     "read:session",
@@ -103,6 +122,7 @@ const activation = {
   findOneValidById,
   markTokenAsUsed,
   activateUserByUserId,
+  EXPIRATION_IN_MILISECONDS,
 };
 
 export default activation;
